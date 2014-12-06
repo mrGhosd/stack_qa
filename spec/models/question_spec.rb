db4615abe912c5b4967452c37edb33f3281d072d
mrGhosd <vforvad@gmail.com>
2014-12-07 00:08:46 +0300

    refactor user and question model specs. Now they are have red state

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ describe Question do
   it { should validate_presence_of :title }
   it { should validate_uniqueness_of :title }
   it { should validate_presence_of :text }
+  it { should have_many :answers }
   it { should belong_to(:user) }
   it { should have_db_index :title }
   it { should have_db_index :is_closed }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ describe User do
   it { should validate_uniqueness_of :email }
   it { should validate_presence_of :password }
   it { should have_many :questions }
+  it { should have_many :answers }
   it { should have_db_index :email }
   it { should have_db_index :password }
 end
