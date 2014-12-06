b8250e799d1ff5ee7f6a216eb1f4f9d4b0d72818
mrGhosd <vforvad@gmail.com>
2014-12-07 00:10:16 +0300

    refactor user and question model specs. Now they have green state

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+  has_many :answers
   validates_presence_of :title, :text
   validates_uniqueness_of :title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User <ActiveRecord::Base
   has_many :questions
+  has_many :answers
   validates_presence_of :email, :password
   validates_uniqueness_of :email
 end
