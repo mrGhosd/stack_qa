b6935747429584d18c3c423404d75768d4fbbc5f
mrGhosd <vforvad@gmail.com>
2015-01-08 04:36:48 +0300

    add pending controller spec for comment

---
+++ spec/controllers/comment_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe CommentsController do
+
+  describe "GET #new" do
+    it "create a new entity of a comment"
+
+    it "render 'new' partial" 
+  end
+end
--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -5,9 +5,16 @@ feature "Unsigned user", :js do
   let!(:question){ create :question, user_id: user.id }
   let!(:comment) { create :comment, user_id: user.id, question_id: question.id }
 
-  scenario "see list of comments" do
+  before do
     visit root_path
     click_link("#{question.title}")
+  end
+
+  scenario "see list of comments" do
     expect(page).to have_content(comment.text)
+  end
+
+  scenario "doesn't see a link for adding a new comment" do
+    expect(page).to_not have_content(".glyphicon.glyphicon-comment")
   end
 end
