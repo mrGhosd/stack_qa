d7b6e0732a145ed9dde1d150406a9fb8842f501d
mrGhosd <vforvad@gmail.com>
2015-01-11 19:51:55 +0300

    create categories table and add red spec for question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108011228) do
+ActiveRecord::Schema.define(version: 20150111164811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20150108011228) do
     t.integer  "question_id"
     t.text     "text"
     t.boolean  "is_helpfull", default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
