6866edf705e209bfb5ddfc5d3b60108bd6ba454b
mrGhosd <vforvad@gmail.com>
2015-01-12 01:29:44 +0300

    add green specs for create action in admin categories controller

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
   has_many :questions
 
   mount_uploader :image, CategoryUploader
+
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
 end
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -17,6 +17,29 @@ describe Admin::CategoriesController do
   end
 
   describe "POST #create" do
-    
+    context "with valid attributes" do
+      it "create a new category" do
+        expect{post :create,
+        category: attributes_for(:category)}.to change(Category, :count).by(1)
+      end
+
+      it "return 200 status" do
+        post :create, category: attributes_for(:category)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't create a new category" do
+        expect{post :create,
+        category: attributes_for(:category, title: "")}.to change(Category, :count).by(0)
+      end
+
+      it "return category errors" do
+        post :create,
+        category: attributes_for(:category, title: "")
+        expect(JSON.parse(response.body)).to have_key('title')
+      end
+    end
   end
 end
