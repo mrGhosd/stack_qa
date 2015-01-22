22cca1e9fa87c2420a46514d6dbc89541724d157
mrGhosd <vforvad@gmail.com>
2015-01-23 00:47:53 +0300

    add specs for user model method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,65 @@ describe User do
   end
 
   describe ".from_omniauth(auth)" do
-    
+    let!(:user) { create :user }
+    let!(:auth) { OmniAuth::AuthHash.new(provider: "facebook", uid: "12345") }
+
+    context "user has already authorized" do
+      it 'return the user' do
+        user.authorizations.create(provider: "facebook", uid: "12345")
+        expect(User.from_omniauth(auth)).to eq(user)
+      end
+    end
+
+    context "user has no authorization" do
+      let!(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: {email: user.email}) }
+
+      it "doesn't create a user" do
+        expect{User.from_omniauth(auth)}.to_not change(User, :count)
+      end
+
+      it 'creates authorization for user' do
+        expect{ User.from_omniauth(auth).to change(user.authorizations, :count).by(1) }
+      end
+
+      it 'creates authorization with provider and uid' do
+        user = User.from_omniauth(auth)
+        authorization = user.authorizations.first
+
+        expect(authorization.provider).to eq auth.provider
+        expect(authorization.uid).to eq auth.uid
+      end
+
+      it 'returns the user' do
+        expect(User.from_omniauth(auth)).to eq(user)
+      end
+    end
+
+    context 'user does not exists' do
+      let!(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: {email: 'new@user.com'}) }
+
+      it 'creates new user' do
+        expect{ User.from_omniauth(auth) }.to change(User, :count).by(1)
+      end
+
+      it 'returns new user' do
+        expect(User.from_omniauth(auth)).to be_a(User)
+      end
+
+      it 'fills user email' do
+        user = User.from_omniauth(auth)
+        expect(user.email).to eq(auth.info.email)
+      end
+
+      it 'creates authorization for user' do
+        user = User.from_omniauth(auth)
+        expect(user.authorizations).to_not be_empty
+      end
+
+      it 'creates authorization with provider and uid' do
+        authorization = User.from_omniauth(auth)
+      end
+    end
   end
+
 end
