8a0b0596dc150c708fbf1c60ddf565ec67377109
mrGhosd <vforvad@gmail.com>
2015-01-24 23:04:46 +0300

    add pundit access specs for edit answer action

--- spec/policies/answer_policy_spec.rb
+++ spec/policies/answer_policy_spec.rb
@@ -46,4 +46,18 @@ describe AnswerPolicy do
       expect(subject).to_not permit(nil, create(:answer))
     end
   end
+
+  permissions :edit? do
+    it "grant access if user is admin?" do
+      expect(subject).to permit(User.new(role: 'admin'), create(:answer))
+    end
+
+    it "grant access if user is author" do
+      expect(subject).to permit(user, create(:answer, user: user))
+    end
+
+    it "denied access if user is not author" do
+      expect(subject).to_not permit(User.new, create(:answer, user: user))
+    end
+  end
 end
