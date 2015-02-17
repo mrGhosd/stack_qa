39e75c39b760014d72e42200b6c678ba6452e44e
mrGhosd <vforvad@gmail.com>
2015-02-17 12:26:39 +0300

    try to implement widget slider

---
+++ app/helpers/questions_helper.rb
@@ -0,0 +1,17 @@
+module QuestionsHelper
+  def widget_filter
+    {
+        last_created: {title: "Последние созданные:", data: questions_filter()},
+        most_commented: {title: "Самые комментируемые", data: },
+        quick_confirmed:{title: "Быстро подтвержденные", data: }
+    }
+  end
+
+  def question_filter(keyword)
+    case keyword
+      when "last_created"
+      when "most_commented"
+      when "quick_confirmed"
+    end
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,18 @@ class Question < ActiveRecord::Base
   has_many :comments, as: :commentable, dependent: :destroy
   validates :title, :text, presence: true
   validates :title, uniqueness: true
+
+  include QuestionsHelper
+
+  def questions_filter(param)
+
+  end
+
+  def self.widget_filter
+    # {
+    #     last_created: {title: "Последние созданные:", data: questions_filter()},
+    #     most_commented: {title: "Самые комментируемые", data: },
+    #     quick_confirmed:{title: "Быстро подтвержденные", data: }
+    # }
+  end
 end
