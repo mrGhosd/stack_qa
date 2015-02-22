e1f251cf1a64c613bd444fdc9bc67f2eb7aeba97
mrGhosd <vforvad@gmail.com>
2015-02-23 02:15:19 +0300

    add collection helper for caching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def collection_key_helper_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s)
+    "#{klass}/collection-#{count}-#{max_updated_at}"
+  end
 end
