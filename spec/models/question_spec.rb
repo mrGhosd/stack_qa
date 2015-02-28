ab78add7f3787ce721d873925edfab849566ee9e
mrGhosd <vforvad@gmail.com>
2015-03-01 02:56:33 +0300

    add scope spec on created_today in question's model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,7 +6,17 @@ describe Question do
   it { should validate_presence_of :text }
   it { should have_many :answers }
   it { should belong_to(:user) }
+  it { should have_many :comments }
+  it { should have_many :question_users }
   it { should belong_to(:category) }
   it { should have_db_index :title }
   it { should have_db_index :is_closed }
+
+  describe "scope .created_today" do
+    let!(:questions_list) { create_list :question, 15 }
+
+    it "return an value of created today questions" do
+      expect(Question.created_today).to eq questions_list
+    end
+  end
 end
