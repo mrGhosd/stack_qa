42f645ee8ef65925f378bd2b942d1575e3f27f1f
mrGhosd <vforvad@gmail.com>
2015-05-13 15:48:52 +0300

    add specs for quesiton api controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -29,8 +29,8 @@ class Api::V1::QuestionsController < Api::ApiController
 
   def show
     question = Question.includes([:comments, :category, :user, :tags]).find(params[:id])
-    current_user_voted = {"current_user_voted" => current_resource_owner.vote_on_question(question) }
-    render json: question.as_json(methods: [:tag_list, :category, :user, :answers_count, :comments_count]).merge(current_user_voted)
+    current_user_voted = {"current_user_voted" => current_resource_owner.vote_on_question(question) } if current_resource_owner
+    render json: question.as_json(methods: [:tag_list, :category, :user, :answers_count, :comments_count]).merge(current_user_voted || {})
   end
 
   def destroy
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -6,8 +6,9 @@ describe "Questions API" do
     it_behaves_like "API Authenticable"
 
     context "authorized" do
+      let!(:category) { create :category }
       let!(:access_token) { create :access_token }
-      let!(:questions) { create_list(:question, 2) }
+      let!(:questions) { create_list(:question, 2, category_id: category.id) }
       let!(:question) { questions.first }
       let!(:answer) { create(:answer, question: question) }
       let!(:comment) { create(:comment, commentable_id: question.id, commentable_type: "Question") }
@@ -23,38 +24,9 @@ describe "Questions API" do
         expect(response.body).to have_json_size(2).at_path("questions")
       end
 
-      %w(id title text created_at updated_at).each do |attr|
+      %w(id title text comments_count tag_list).each do |attr|
         it "question object contains #{attr}" do
           expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("questions/0/#{attr}")
-        end
-      end
-
-      it 'question object contains short title' do
-        expect(response.body).to be_json_eql(question.title.truncate(10).to_json).at_path("questions/0/short_title")
-      end
-
-      context 'answers' do
-
-        it 'included in question object' do
-          expect(response.body).to have_json_size(1).at_path('questions/0/answers')
-        end
-
-        %w(id text created_at updated_at).each do |attr|
-          it "contains #{attr}" do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{attr}")
-          end
-        end
-      end
-
-      context 'comments' do
-        it 'included in question object' do
-          expect(response.body).to have_json_size(1).at_path('questions/0/comments')
-        end
-
-        %w(id text created_at updated_at).each do |attr|
-          it "contains #{attr}" do
-            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("questions/0/comments/0/#{attr}")
-          end
         end
       end
     end
@@ -70,14 +42,14 @@ describe "Questions API" do
         expect{
           post "api/v1/questions", format: :json,
                access_token: access_token.token,
-               question: attributes_for(:question, category: category, user: user)
+               question: attributes_for(:question, category_id: category.id, user: user)
         }.to change(Question, :count).by(1)
       end
 
       it "return just create question" do
         post "api/v1/questions", format: :json,
              access_token: access_token.token,
-             question: attributes_for(:question, category: category, user: user)
+             question: attributes_for(:question, category_id: category.id, user: user)
         expect(response.body).to be_json_eql(Question.last.to_json)
       end
     end
@@ -87,16 +59,101 @@ describe "Questions API" do
         expect { post "api/v1/questions", format: :json,
                 access_token: access_token.token,
                 question: attributes_for(:question,
-                title: "", category: category, user: user) }.to change(Question, :count).by(0)
+                title: "", category_id: category.id, user: user) }.to change(Question, :count).by(0)
       end
 
       it "return array of json errors" do
         post "api/v1/questions", format: :json,
              access_token: access_token.token,
              question: attributes_for(:question,
-             title: "", category: category, user: user)
+             title: "", category_id: category.id, user: user)
         expect(JSON.parse(response.body)).to have_key("title")
       end
     end
+  end
+
+  describe "GET #show" do
+    let!(:category) { create :category }
+    let!(:user) { create :user }
+    let!(:question) { create :question, category_id: category.id, user_id: user.id }
+    # login_user
+
+    before { get "api/v1/questions/#{question.id}" }
+
+    %w(id title text comments_count tag_list answers_count category user).each do |attr|
+      it "question object contains #{attr}" do
+        expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("#{attr}")
+      end
+    end
+
+    it "return 200 status" do
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "PUT #update" do
+    let!(:user) { create :user }
+    let!(:access_token) { create :access_token }
+    let!(:category) { create :category }
+    let!(:question) { create :question, user_id: user.id, category_id: category.id }
+
+    context "with valid attributes" do
+      it "update question" do
+        put "api/v1/questions/#{question.id}", format: :json,
+            id: question.id,
+            access_token: access_token.token,
+            question: attributes_for(:question, user_id: user.id, category_id: category.id, title: "2")
+        question.reload
+        expect(question.title).to eq("2")
+      end
+
+      it "return just updated question" do
+        put "api/v1/questions/#{question.id}", format: :json,
+            id: question.id,
+            access_token: access_token.token,
+            question: attributes_for(:question, user_id: user.id, category_id: category.id, title: "2")
+        question.reload
+        expect(response.body).to eq(question.to_json)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't update question" do
+        put "api/v1/questions/#{question.id}", format: :json,
+            id: question.id,
+            access_token: access_token.token,
+            question: attributes_for(:question, user_id: user.id, category_id: category.id, title: "")
+        question.reload
+        expect(question.title).to eq(question.title)
+      end
+
+      it "return json error" do
+        put "api/v1/questions/#{question.id}", format: :json,
+            id: question.id,
+            access_token: access_token.token,
+            question: attributes_for(:question, user_id: user.id, category_id: category.id, title: "")
+        question.reload
+        expect(JSON.parse(response.body)).to have_key("title")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:user) { create :user }
+    let!(:access_token) { create :access_token }
+    let!(:category) { create :category }
+    let!(:question) { create :question, user_id: user.id, category_id: category.id }
+
+    it "delete question" do
+      expect{ delete "api/v1/questions/#{question.id}", format: :json,
+      id: question.id, access_token: access_token.token }.to change(Question, :count).by(-1)
+    end
+
+    it "return success status" do
+      delete "api/v1/questions/#{question.id}", format: :json,
+             id: question.id, access_token: access_token.token
+      expect(response.status).to eq(200)
+    end
+
   end
 end
