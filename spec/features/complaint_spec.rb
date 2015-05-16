a80c067a911ae30c4b85988d6437f44b0f1b535c
mrGhosd <vforvad@gmail.com>
2015-05-16 19:30:31 +0300

    add failure feature spec for complaint creation

---
+++ spec/features/complaint_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "User", :js do
+  let!(:user) { create :user }
+  let!(:category) { create :category }
+  let!(:question) { create :question, user_id: user.id, category_id: category.id }
+
+  context "Question" do
+    before do
+      sign_in user
+    end
+
+    scenario "create complaint" do
+      visit question_path(question)
+      find('complain-question', match: :first).click
+      sleep 0.2
+      expect(page).to have_css(".complain_message")
+    end
+  end
+end
