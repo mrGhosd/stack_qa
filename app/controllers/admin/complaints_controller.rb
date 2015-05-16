d241e30aee0989972e5303c84b8180268c4a90e7
mrGhosd <vforvad@gmail.com>
2015-05-16 21:58:27 +0300

    add gree specs for admin complaints controller index action

--- app/controllers/admin/complaints_controller.rb
+++ app/controllers/admin/complaints_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ComplaintsController < AdminController
   def index
-    
+    @complaints = Complaint.paginate(page: params[:page] || 1, per_page: 20).order(created_at: :desc)
   end
 end
--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,4 +1,17 @@
 class Complaint < ActiveRecord::Base
   belongs_to :user
   belongs_to :complaintable, polymorphic: true, touch: true
+
+  def parent
+    case self.commentable_type
+      when "Question"
+        Question.find(self.commentable_id)
+      when "Answer"
+        Answer.find(self.commentable_id)
+      when "Comment"
+        Comment.find(self.commentable_id)
+      else
+        nil
+    end
+  end
 end
