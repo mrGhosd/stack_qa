c5ec395a4886b23f33d09450b8ee378f94828f8c
vsokoltsov <vforvad@gmail.com>
2015-06-11 20:01:00 +0300

    update .env gem

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Stackqa
   class Application < Rails::Application
+    # Load defaults from config/*.env in config
+    Dotenv.load *Dir.glob(Rails.root.join("config/**/*.env"), File::FNM_DOTMATCH)
+
+  # Override any existing variables if an environment-specific file exists
+    Dotenv.overload *Dir.glob(Rails.root.join("config/**/*.env.#{Rails.env}"), File::FNM_DOTMATCH)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
